32238a36b331d30e5d91ac7dc8bca80d7234f094
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-10-27 20:07:13 +0100

    Fix Flight model in models.py in airport app / add migrations

--- airport/models.py
+++ airport/models.py
@@ -50,7 +50,7 @@ class Airplane(models.Model):
 class Flight(models.Model):
     route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="flights")
     airplane = models.ForeignKey(
-        AirplaneType, on_delete=models.CASCADE, related_name="flights"
+        Airplane, on_delete=models.CASCADE, related_name="flights"
     )
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
